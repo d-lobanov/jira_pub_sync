8edd9965c77b05bb3a437289d921e0d935ba1bbe
Dmitry Lobanov <d.lobanov@itransition.com>
2016-12-20 23:10:17 +0300

    + Init git module

---
+++ git.py
@@ -0,0 +1,16 @@
+from src import config
+from src.io import IO
+from src.synchronizer import Synchronizer as Sync
+import click
+import pytz
+
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
+
+
+def main():
+    pass
+
+main()
